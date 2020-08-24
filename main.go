efe3d5cf4ad0682564de0fb61dbff8832e952338
PaulGureghian <paulgureghian@att.net>
2020-08-24 14:49:59 -0700

    Added query functions

--- main.go
+++ main.go
@@ -1,10 +1,58 @@
 package main
 
-var cache = map[int]
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-
-
+var cache = map[int]Book{}
+var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 
+	for i := 0; i < 10; i++ {
+
+		id := rnd.Intn(10) + 1
+
+		if b, ok := queryCache(id); ok {
+
+			fmt.Println("Found in cache")
+			fmt.Println(b)
+			continue
+		}
+
+		if b, ok := queryDatabase(id); ok {
+
+			fmt.Println("Found in database")
+			fmt.Println(b)
+			continue
+		}
+
+		fmt.Println("Book not found with id: '%v'", id)
+		time.Sleep(150 * time.Millisecond)
+	}
+}
+
+// Add query functions
+func queryCache(id int) (Book, bool) {
+
+	b, ok := cache[id]
+
+	return b, ok
+}
+
+func queryDatabase(id int) (Book, bool) {
+
+	time.Sleep(100 * time.Millisecond)
+
+	for _, b := range books {
+
+		if b.ID == id {
+			cache[id] = b
+			return b, true
+		}
+	}
+
+	return Book{}, false
 }
